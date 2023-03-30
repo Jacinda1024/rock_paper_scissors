5026868b4b1d4d8733cb18799f6ca38968b8c048
Jacinda <118142578+Jacinda1024@users.noreply.github.com>
2023-03-29 21:24:29 -0500

    Started html and script

---
+++ script.js
@@ -0,0 +1,17 @@
+// get computers choice
+function getComputerChoice (){
+    let randomNumber = Math.floor(Math.random()*3)
+    switch(randomNumber){
+        case 0:
+            return 'rock'
+        case 1:
+            return 'scissors'
+        case 2:
+            return 'paper'
+        default:
+            return 'Select a valid option'
+    }
+}
+
+
+
